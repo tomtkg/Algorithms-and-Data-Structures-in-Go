e42569c7363a211ffce5679ad03b706a5ca875cf
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-04-03 03:45:38 +0900

    Update chapter4

---
+++ chapter4/quick_sortA_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func Test_quickSortA1(t *testing.T) {
+	seq := []int{37, 33, 29, 25, 22, 20, 19, 14, 10, 8}
+	want := []int{8, 10, 14, 19, 20, 22, 25, 29, 33, 37}
+	quickSortA(seq, 0, len(seq)-1)
+	for i := range seq {
+		if seq[i] != want[i] {
+			t.Errorf("seq[%d] = %v, want %v", i, seq[i], want[i])
+		}
+	}
+}
+func Test_quickSortA2(t *testing.T) {
+	seq := []int{6, 9, 12, 7, 15, 23, 2, 10, 4, 20}
+	want := []int{2, 4, 6, 7, 9, 10, 12, 15, 20, 23}
+	quickSortA(seq, 0, len(seq)-1)
+	for i := range seq {
+		if seq[i] != want[i] {
+			t.Errorf("seq[%d] = %v, want %v", i, seq[i], want[i])
+		}
+	}
+}
+func Test_quickSortA3(t *testing.T) {
+	seq := []int{6, 9, 3, 8, 7, 5, 4, 2, 1}
+	want := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	quickSortA(seq, 0, len(seq)-1)
+	for i := range seq {
+		if seq[i] != want[i] {
+			t.Errorf("seq[%d] = %v, want %v", i, seq[i], want[i])
+		}
+	}
+}
