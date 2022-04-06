97cb13b6e12274e5f76027efc1cdbe9c68dacf02
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-04-06 09:25:10 +0900

    Update polish_notation

--- chapter5/04polish_notation/main_test.go
+++ chapter5/04polish_notation/main_test.go
@@ -4,12 +4,21 @@ import (
 	"os"
 )
 
-func Example_main() {
-	for _, v := range []string{"a", "b"} {
-		os.Stdin, _ = os.Open(v + ".txt")
-		main()
-	}
+func Example_mainA() {
+	r, w, _ := os.Pipe()
+	w.Write([]byte("*\n1.2\n+\n3.4\n1.6"))
+	w.Close()
+	os.Stdin = r
+	main()
 	// Output:
 	// 6.00
+}
+func Example_mainB() {
+	r, w, _ := os.Pipe()
+	w.Write([]byte("+\n-0.1\n*\n-\n2.5\n4.7\n+\n1.7\n2.8"))
+	w.Close()
+	os.Stdin = r
+	main()
+	// Output:
 	// -10.00
 }
