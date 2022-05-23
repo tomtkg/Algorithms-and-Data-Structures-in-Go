cdde226a43cdeaf60276b0903395a678966f791c
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-05-23 23:32:41 +0900

    Create 01binary_tree

---
+++ chapter8/01binary_tree/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+func Example_main() {
+	main()
+	// Output:
+	// -- Binary tree --
+	// 	{Steven 1994}
+	// {Gary 1997}
+	// 		{Upton 1988}
+	// 	{Tyler 1985}
+	// 		{Martin 1973}
+}
