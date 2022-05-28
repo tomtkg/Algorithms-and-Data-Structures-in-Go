d083c0a1ace6ebff1e4d763a9249184849ef1b42
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-05-28 11:58:07 +0900

    Create 06pbtree

---
+++ chapter8/06pbtree/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+func Example_mainA() {
+	main()
+	// Output:
+	// -- Generated binary tree --
+	// 		11
+	// 	12
+	// 		13
+	// 			14
+	// 15
+	// 		16
+	// 			17
+	// 	18
+	// 		19
+	// 			20
+}
