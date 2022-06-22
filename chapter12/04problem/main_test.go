c50efc2fbb88b7b209d01de6f162ab0d863ac688
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-06-22 17:32:37 +0900

    Update main_test.go

--- chapter12/04problem/main_test.go
+++ chapter12/04problem/main_test.go
@@ -3,7 +3,8 @@ package main
 func Example_main() {
 	main()
 	// Output:
-	// [50 11 32 3 4 34 39 0 0 19]
-	// [50 11 32 3 4 0 16 36 0 0 0]
-	// [50 11 32 3 4 0 16 36]
+	// 406->407->408->409
+	// 406->743
+	// STEAK:378, HAMBURG:379, HUNGRY:383,
+	// STEAK:803, HAMBURG:40, HUNGRY:378,
 }
