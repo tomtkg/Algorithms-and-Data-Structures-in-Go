b2516cb486a5e7a7ac29f8cee88a62641c8da07c
tomtkg <39430259+tomtkg@users.noreply.github.com>
2022-09-02 13:31:24 +0900

    Update mylib.go

--- mylib/mylib.go
+++ mylib/mylib.go
@@ -2,8 +2,18 @@ package mylib
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"reflect"
+	"runtime"
+	"strings"
 )
+
+func PrintFuncName(f interface{}) {
+	name := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+	arr := strings.Split(name, ".")
+	fmt.Println(arr[len(arr)-1])
+}
 
 func SetStdin(x interface{}) {
 	s, _ := json.Marshal(x)
